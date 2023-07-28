9fe301906e375246a14af06e12dd751f1833f7e1
adam <adam@myworld.net>
2023-07-28 18:07:17 +0200

    minor

--- modes/lookup/main.py
+++ modes/lookup/main.py
@@ -11,7 +11,7 @@ from plugin.app import register
 from plugin.app.mode import AppMode
 from plugin.widget import InputListStack, ListWidget, Icon
 
-# from .translate import en_translation, de_translation
+from .translate import en_translation, de_translation
 
 class LookupMode(AppMode):
 
